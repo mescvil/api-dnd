43eb45592ae2f211edf31302b308771390bbfb34
RafaelRusRuiz <rusruizrafael@outlook.es>
2023-03-17 17:28:23 +0100

    Creación BBDD Criaturas, Insercción Enemigos,Rasgos,Acciones, Inserccion Relaciones

---
+++ SQL/Relaciones.sql
@@ -0,0 +1,424 @@
+use criaturas;
+/*Planetar*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Planetar'),
+('Velocidad a Vuelo 2','Planetar'),
+('Resistencia a Contundente','Planetar'),
+('Resistencia a Cortante','Planetar'),
+('Resistencia a Perforante','Planetar'),
+('Inmunidad a Asustado','Planetar'),
+('Inmunidad a Cansancio','Planetar'),
+('Inmunidad a Hechizado','Planetar'),
+('Armas Angelicales','Planetar'),
+('Percepción Divina','Planetar'),
+('Lanzamiento de Conjuros Innato','Planetar'),
+('Resistencia Mágica','Planetar');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque múltiple 1','Planetar'),
+('Espadón','Planetar'),
+('Toque Sanador','Planetar');
+
+/*Aparicion*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 2','Aparicion'),
+('Resistencia a Contundente','Aparicion'),
+('Resistencia a Cortante','Aparicion'),
+('Resistencia a Perforante','Aparicion'),
+('Resistencia a Ácido','Aparicion'),
+('Resistencia a Frío','Aparicion'),
+('Resistencia a Fuego','Aparicion'),
+('Resistencia a Relámpago','Aparicion'),
+('Resistencia a Trueno','Aparicion'),
+('Inmunidad a Necrótico','Aparicion'),
+('Inmunidad a Veneno','Aparicion'),
+('Inmunidad a Agarrado','Aparicion'),
+('Inmunidad a Apresado','Aparicion'),
+('Inmunidad a Cansancio','Aparicion'),
+('Inmunidad a Derribado','Aparicion'),
+('Inmunidad a Envenenado','Aparicion'),
+('Inmunidad a Hechizado','Aparicion'),
+('Inmunidad a Paralizado','Aparicion'),
+('Inmunidad a Petrificado','Aparicion'),
+('Movimiento Incorporeo','Aparicion'),
+('Sensibilidad a la luz Solar','Aparicion');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Consumir Vida','Aparicion'),
+('Crear Espectro','Aparicion');
+
+/*Azotamentes*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Azotamentes'),
+('Resistencia Mágica','Azotamentes'),
+('Lanzamiento de Conjuros Innato','Azotamentes');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Tentáculos 2','Azotamentes'),
+('Extraer Cerebro','Azotamentes'),
+('Descarga Mental','Azotamentes');
+
+/*Behir*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 2','Behir'),
+('Velocidad al Trepar 1','Behir'),
+('Resistencia a Relámpago','Behir');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Behir'),
+('Mordisco 2','Behir'),
+('Constreñir','Behir'),
+('Aliento de Relámpago','Behir'),
+('Engullir','Behir');
+
+/*Bestia de Trémula*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Bestia de Trémula'),
+('Evitar','Bestia de Trémula'),
+('Desplazamiento','Bestia de Trémula');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Bestia de Trémula'),
+('Tentáculos 1','Bestia de Trémula');
+
+/*Broza movediza*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Broza movediza'),
+('Velocidad a Nado 1','Broza movediza'),
+('Resistencia a Frío','Broza movediza'),
+('Resistencia a Fuego','Broza movediza'),
+('Inmunidad a Relámpago','Broza movediza'),
+('Inmunidad a Cansancio','Broza movediza'),
+('Inmunidad a Cegado','Broza movediza'),
+('Inmunidad a Ensordecido','Broza movediza'),
+('Absorción de Relámpago','Broza movediza');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Broza movediza'),
+('Golpe 1','Broza movediza'),
+('Absorber','Broza movediza');
+
+/*Cocatriz*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Cocatriz'),
+('Velocidad a Vuelo 1','Cocatriz');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Mordisco Petrificante','Cocatriz');
+
+/*Caballero de la Muerte*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Caballero de la Muerte'),
+('Inmunidad a Necrótico','Caballero de la Muerte'),
+('Inmunidad a Veneno','Caballero de la Muerte'),
+('Inmunidad a Asustado','Caballero de la Muerte'),
+('Inmunidad a Cansancio','Caballero de la Muerte'),
+('Inmunidad a Envenenado','Caballero de la Muerte'),
+('Resistencia Mágica','Caballero de la Muerte'),
+('Comandar Muertos Vivientes','Caballero de la Muerte'),
+('Lanzamiento de Conjuros Innato','Caballero de la Muerte');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque múltiple 2','Caballero de la Muerte'),
+('Espada Larga','Caballero de la Muerte'),
+('Orbe de Fuego Infernal','Caballero de la Muerte');
+
+/*Contemplador*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 1','Contemplador'),
+('Inmunidad a Derribado','Contemplador'),
+('Cono Antimagia','Contemplador');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Mordisco 2','Contemplador'),
+('Rayo Ocular Cautivador','Contemplador'),
+('Rayo Ocular Paralizador','Contemplador'),
+('Rayo Ocular Aterrador','Contemplador'),
+('Rayo Ocular Ralentizador','Contemplador'),
+('Rayo Ocular Debilitador','Contemplador'),
+('Rayo Ocular Telequinético','Contemplador'),
+('Rayo Ocular Petrificador','Contemplador'),
+('Rayo Ocular Somnifero','Contemplador'),
+('Rayo Ocular Desintegrador','Contemplador'),
+('Rayo Ocular Mortal','Contemplador');
+
+/*Dríade*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Dríade'),
+('Lanzamiento de Conjuros Innato','Dríade'),
+('Resistencia Mágica','Dríade'),
+('Hablar con Bestias y Plantas','Dríade'),
+('Paso Árboreo','Dríade');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Garrote','Dríade'),
+('Encanto Feérico','Dríade');
+
+/*Hezrou*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Hezrou'),
+('Inmunidad a Veneno','Hezrou'),
+('Inmunidad a Envenenado','Hezrou'),
+('Resistencia a Contundente','Hezrou'),
+('Resistencia a Cortante','Hezrou'),
+('Resistencia a Perforante','Hezrou'),
+('Resistencia a Frío','Hezrou'),
+('Resistencia a Fuego','Hezrou'),
+('Resistencia a Relámpago','Hezrou'),
+('Resistencia Mágica','Hezrou'),
+('Hedor','Hezrou');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 2','Hezrou'),
+('Mordisco 2','Hezrou'),
+('Garra 2','Hezrou');
+
+/*Anjanath*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 2','Anjanath');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Anjanath'),
+('Mordisco 2','Anjanath'),
+('Cola 1','Anjanath');
+
+/*Elemental de Aire*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 2','Elemental de Aire'),
+('Inmunidad a Veneno','Elemental de Aire'),
+('Inmunidad a Agarrado','Elemental de Aire'),
+('Inmunidad a Apresado','Elemental de Aire'),
+('Inmunidad a Cansancio','Elemental de Aire'),
+('Inmunidad a Derribado','Elemental de Aire'),
+('Inmunidad a Envenenado','Elemental de Aire'),
+('Inmunidad a Inconsciente','Elemental de Aire'),
+('Inmunidad a Paralizado','Elemental de Aire'),
+('Inmunidad a Petrificado','Elemental de Aire'),
+('Resistencia a Relámpago','Elemental de Aire'),
+('Resistencia a Trueno','Elemental de Aire'),
+('Resistencia a Contundente','Elemental de Aire'),
+('Resistencia a Cortante','Elemental de Aire'),
+('Resistencia a Perforante','Elemental de Aire'),
+('Forma de Aire','Elemental de Aire');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Elemental de Aire'),
+('Golpe 2','Elemental de Aire'),
+('Torbellino','Elemental de Aire');
+
+/*Estirge*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 1','Estirge'),
+('Velocidad a Pie 1','Estirge');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Chupar Sangre','Estirge');
+
+/*Espectro*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 2','Espectro'),
+('Resistencia a Contundente','Espectro'),
+('Resistencia a Cortante','Espectro'),
+('Resistencia a Perforante','Espectro'),
+('Resistencia a Ácido','Espectro'),
+('Resistencia a Frío','Espectro'),
+('Resistencia a Fuego','Espectro'),
+('Resistencia a Relámpago','Espectro'),
+('Resistencia a Trueno','Espectro'),
+('Inmunidad a Necrótico','Espectro'),
+('Inmunidad a Veneno','Espectro'),
+('Inmunidad a Agarrado','Espectro'),
+('Inmunidad a Apresado','Espectro'),
+('Inmunidad a Cansancio','Espectro'),
+('Inmunidad a Derribado','Espectro'),
+('Inmunidad a Envenenado','Espectro'),
+('Inmunidad a Hechizado','Espectro'),
+('Inmunidad a Inconsciente','Espectro'),
+('Inmunidad a Paralizado','Espectro'),
+('Inmunidad a Petrificado','Espectro'),
+('Movimiento Incorporeo','Espectro'),
+('Sensibilidad a la luz Solar','Espectro');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Consumir Vida','Espectro');
+
+/*Flumph*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 1','Flumph'),
+('Vulnerabilidad a Psíquico','Flumph'),
+('Telepatía Avanzada','Flumph'),
+('Vulnerable a ser Derribado','Flumph'),
+('Velo Telepático','Flumph');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Zarcillos','Flumph'),
+('Rociada Apestosa','Flumph');
+
+/*Fomoré*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Fomoré');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Fomoré'),
+('Garrote Grande','Fomoré'),
+('Mal de Ojo','Fomoré'),
+('Maldición Mal de Ojo','Fomoré');
+
+/*Fuego Fatuo*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Vuelo 2','Fuego Fatuo'),
+('Inmunidad a Relámpago','Fuego Fatuo'),
+('Inmunidad a Veneno','Fuego Fatuo'),
+('Resistencia a Ácido','Fuego Fatuo'),
+('Resistencia a Frío','Fuego Fatuo'),
+('Resistencia a Fuego','Fuego Fatuo'),
+('Resistencia a Necrótico','Fuego Fatuo'),
+('Resistencia a Trueno','Fuego Fatuo'),
+('Resistencia a Contundente','Fuego Fatuo'),
+('Resistencia a Cortante','Fuego Fatuo'),
+('Resistencia a Perforante','Fuego Fatuo'),
+('Inmunidad a Agarrado','Fuego Fatuo'),
+('Inmunidad a Apresado','Fuego Fatuo'),
+('Inmunidad a Cansancio','Fuego Fatuo'),
+('Inmunidad a Derribado','Fuego Fatuo'),
+('Inmunidad a Envenenado','Fuego Fatuo'),
+('Inmunidad a Inconsciente','Fuego Fatuo'),
+('Inmunidad a Paralizado','Fuego Fatuo'),
+('Devorar Vida','Fuego Fatuo'),
+('Efímero','Fuego Fatuo'),
+('Movimiento Incorpóreo','Fuego Fatuo'),
+('Iluminación Variable','Fuego Fatuo');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Descarga','Fuego Fatuo'),
+('INvisibilidad','Fuego Fatuo');
+
+/*Garra Reptante*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Garra Reptante'),
+('Velocidad al Trepar 1','Garra Reptante'),
+('Inmunidad a Veneno','Garra Reptante'),
+('Inmunidad a Cansancio','Garra Reptante'),
+('Inmunidad a Envenenado','Garra Reptante'),
+('Inmunidad a Hechizado','Garra Reptante'),
+('Inmunidad a Expulsión','Garra Reptante');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Garra 1','Garra Reptante');
+
+/*Gigante de Piedra*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Gigante de Piedra'),
+('Camuflaje con la Piedra','Gigante de Piedra');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Gigante de Piedra'),
+('Garrote Grande','Gigante de Piedra'),
+('Piedra','Gigante de Piedra');
+
+/*Guardian Escudo*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Guardian Escudo'),
+('Inmunidad a Veneno','Guardian Escudo'),
+('Inmunidad a Asustado','Guardian Escudo'),
+('Inmunidad a Cansancio','Guardian Escudo'),
+('Inmunidad a Envenenado','Guardian Escudo'),
+('Inmunidad a Hechizado','Guardian Escudo'),
+('Inmunidad a Paralizado','Guardian Escudo'),
+('Vínculo','Guardian Escudo'),
+('Regeneración','Guardian Escudo');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Guardian Escudo'),
+('Puñetazo','Guardian Escudo');
+
+/*Hidra*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Hidra'),
+('Velocidad a Nado 1','Hidra'),
+('Aguantar la Respiración','Hidra'),
+('Multiples Cabezas','Hidra'),
+('Cabezas Reactivas','Hidra'),
+('Vigilante','Hidra');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 2','Hidra'),
+('Mordisco 1','Hidra');
+
+/*Horror Ganchudo*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Horror Ganchudo'),
+('Velocidad al Trepar 1','Horror Ganchudo'),
+('Ecolocalización','Horror Ganchudo'),
+('Oído Agudo','Horror Ganchudo');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Horror Ganchudo'),
+('Garfio','Horror Ganchudo');
+
+/*Horror Acorazado*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Horror Acorazado'),
+('Velocidad a Vuelo 1','Horror Acorazado'),
+('Resistencia a Contundente','Horror Acorazado'),
+('Resistencia a Cortante','Horror Acorazado'),
+('Resistencia a Perforante','Horror Acorazado'),
+('Inmunidad a Necrótico','Horror Acorazado'),
+('Inmunidad a Veneno','Horror Acorazado'),
+('Inmunidad a Asustado','Horror Acorazado'),
+('Inmunidad a Aturdido','Horror Acorazado'),
+('Inmunidad a Cegado','Horror Acorazado'),
+('Inmunidad a Ensordecido','Horror Acorazado'),
+('Inmunidad a Envenenado','Horror Acorazado'),
+('Inmunidad a Hechizado','Horror Acorazado'),
+('Inmunidad a Paralizado','Horror Acorazado'),
+('Inmunidad a Petrificado','Horror Acorazado'),
+('Resistencia Mágica','Horror Acorazado');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 1','Horror Acorazado'),
+('Espada Larga','Horror Acorazado');
+
+/*Kobold*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Kobold'),
+('Sensibilidad a la Luz Solar','Kobold'),
+('Ataque en Manada','Kobold');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Daga','Kobold'),
+('Honda','Kobold');
+
+/*Kraken*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Kraken'),
+('Velocidad a Nado 2','Kraken'),
+('Inmunidad a Relámpago','Kraken'),
+('Inmunidad a Contundente','Kraken'),
+('Inmunidad a Cortante','Kraken'),
+('Inmunidad a Perforante','Kraken'),
+('Inmunidad a Asustado','Kraken'),
+('Inmunidad a Paralizado','Kraken'),
+('Anfibio','Kraken'),
+('Libertad de Movimiento','Kraken'),
+('Monstruo de Asedio','Kraken');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Ataque Múltiple 2','Kraken'),
+('Mordisco 2','Kraken'),
+('Tentáculos 2','Kraken'),
+('Tormenta de Relámpagos','Kraken');
+
+/*Kuo-Toa*/
+insert into rasgos_enemigos(nombre_rasgo,nombre_enemigo) values
+('Velocidad a Pie 1','Kuo-Toa'),
+('Velocidad a Nado 1','Kuo-Toa'),
+('Anfibio','Kuo-Toa'),
+('Percepción Sobrenatural','Kuo-Toa'),
+('Escurridizo','Kuo-Toa'),
+('Sensibilidad a la Luz Solar','Kuo-Toa');
+
+insert into acciones_enemigos(nombre_accion,nombre_enemigo) values
+('Mordisco 1','Kuo-Toa'),
+('Lanza','Kuo-Toa'),
+('Red','Kuo-Toa');
